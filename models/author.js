309822665cfb81e6332798b8b69eb5de3c159a03
harmolipi <nikob381@gmail.com>
2022-02-14 12:56:36 -0500

    Formatted virtual lifespan with luxon

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
@@ -21,19 +22,14 @@ AuthorSchema.virtual('name').get(function () {
 
 // Virtual for author's lifespan
 AuthorSchema.virtual('lifespan').get(function () {
-  let lifetime_string = '';
+  const birth = this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : '';
+  const death = this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : 'present';
 
-  if (this.date_of_birth) {
-    lifetime_string = this.date_of_birth.getYear().toString();
-  }
-
-  lifetime_string += ' - ';
-
-  if (this.date_of_death) {
-    lifetime_string += this.date_of_death.getYear().toString();
-  }
-
-  return lifetime_string;
+  return `${birth} - ${death}`;
 });
 
 // Virtual for author's URL
